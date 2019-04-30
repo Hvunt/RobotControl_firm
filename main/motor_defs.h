a762d838997d256e0b4f84ba31acf70011163821
hvunt <hvunt32@gmail.com>
2019-04-30 17:46:56 +0300

    added motor defines

---
+++ main/motor_defs.h
@@ -0,0 +1,25 @@
+
+#define MOTORS_FORWARD "FORWARD"
+#define MOTORS_REVERSE "REVERSE"
+#define MOTORS_LEFT "LEFT"
+#define MOTORS_RIGHT "RIGHT"
+#define MOTORS_STOP "STOP"
+
+/*
+ * Commands definitions
+ */
+//SET
+#define COMM_MOVE				0x50
+#define COMM_SET_SERVOS_POS		0x80
+
+//GET
+#define COMM_GET_SPEED			0x70
+#define COMM_GET_SERVOS_POS 	0xB0
+
+enum {
+	MOVE_FORWARD = 0x02,
+	MOVE_REVERSE = 0x04,
+	MOVE_LEFT = 0x20,
+	MOVE_RIGHT = 0x22,
+	MOVE_STOP = 0x44
+};
