5211933adb6f130ea52913489934b2b9c88a2559
Sergey Popov <hvunt32@gmail.com>
2019-05-06 23:19:42 +0300

    added lpa_star files

---
+++ main/list.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "list.h"
+
+void list_add(list_t **list, node_t *node){
+    list_t *new_node = (list_t *) malloc(sizeof(list_t));
+    new_node->nodes = node;
+    new_node->next = *list;
+    *list = new_node;
+}
